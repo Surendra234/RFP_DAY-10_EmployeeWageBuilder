f9546f18d8a6464448f32b1f3c7b619c4bfb400d
Surendra234 <surendramahawar873@gmail.com>
2022-06-08 01:23:46 +0530

    EmployeeWageBuilder

--- src/com/bridgelabz/EmployeeWageBuilder.java
+++ src/com/bridgelabz/EmployeeWageBuilder.java
@@ -1,19 +1,31 @@
 package com.bridgelabz;
 
 public class EmployeeWageBuilder {
+    // Constant
     static final int isFullTime = 1;
+    static final int empRatePerHour = 20;
 
     public static void main(String[] args) {
-        empCheck();
+        dailyEmployeeWage();
     }
 
-    // Method to check employee is present or absent by using random function
-    public static void empCheck() {
+    // Method to Calculate daily employee wage
+    public static void dailyEmployeeWage() {
 
-        double empCheck = Math.floor(Math.random() * 10) % 2;
-            if (empCheck == isFullTime)
-                System.out.println("Employee is present !");
-            else
-                System.out.println("Employee is absent !");
+        // Variables
+        int empHour = 0;
+        int empWage = 0;
+        double empCheck = Math.floor(Math.random() * 10 ) % 2;
+
+        if(empCheck == isFullTime) {
+            System.out.println("Employee is present !");
+            empHour = 8;
+        }
+        else {
+            System.out.println("Employee is absent !");
+            empHour = 0;
+        }
+        empWage = empHour * empRatePerHour;
+        System.out.println("Daily Employee Wage = " +empWage);
     }
 }
